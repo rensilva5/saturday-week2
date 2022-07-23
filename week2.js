0822d2349021dad913b6f3ccaf8941f904d91806
Renzo Silva <renzoasilva@gmail.com>
2022-07-23 11:46:42 -0400

    first exercice done

---
+++ week2.js
@@ -0,0 +1,20 @@
+let students = [
+    {
+        name: "maxx",
+        here: true,
+    },
+    {
+        name: "pam",
+        here: true
+    }
+]
+
+function getStudentsNames (studentsArray) {
+    let studentName = []
+    for (let i = 0; i < studentsArray.length; i++) {
+        studentName.push(studentsArray[i].name)
+
+    }
+    return studentName
+}
+console.log(getStudentsNames(students))
